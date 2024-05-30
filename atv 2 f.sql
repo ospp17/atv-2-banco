701ae2d904125f7df7fbd50f3ef4a10f1cecaba8
octavio scremin <134084015+ospp17@users.noreply.github.com>
2024-05-30 18:22:17 -0300

    3

--- atv 2 f.sql
+++ atv 2 f.sql
@@ -9,3 +9,42 @@ VALUES ('Cem Anos de Solidão', 'Gabriel Garcia Marque', 1967, 'Ficçao', 978-02
 UPDATE Livros
 SET disponibilidade = FALSE
 WHERE ano_publicacao < 2000;
+
+UPDATE Livros
+SET editora = 'Plume Books'
+WHERE titulo = '1984';
+
+UPDATE Livros
+SET idioma = 'Inglês'
+WHERE editora = 'Penguin Books';
+
+UPDATE Livros
+SET titulo = 'Harry Potter e a Pedra Filosofal (Edição Especial)'
+WHERE isbn = '978-0439708180';
+
+DELETE FROM Livros
+WHERE genero = 'Terror';
+
+DELETE FROM Livros
+WHERE idioma = 'Francês' AND ano_publicacao < 1970;
+
+DELETE FROM Livros
+WHERE titulo = 'As Crônicas de Nárnia';
+
+DELETE FROM Livros
+WHERE editora = 'Penguin Books';
+
+SELECT *
+FROM Livros
+WHERE numero_de_paginas > 500;
+
+SELECT genero, COUNT(*) AS quantidade_de_livros
+FROM Livros
+GROUP BY genero;
+
+SELECT 
+COUNT(*) AS total_de_livros,
+AVG(numero_de_paginas) AS media_de_paginas
+FROM Livros
+WHERE genero = 'Drama';
+
