7ebcaec3e2a63bd119a7ef1b4d5f67f5bad271a4
octavio scremin <134084015+ospp17@users.noreply.github.com>
2024-05-30 18:01:17 -0300

    1
    1

--- atv 2 f.sql
+++ atv 2 f.sql
@@ -1,1 +1,8 @@
-......
+INSERT INTO Livros (titulo, autor, ano_publicacao, disponivel, categoria, isbn, editora, quantidade_paginas, idioma)
+VALUES ('As Crônicas de Nárnia', 'C.S. Lewis', 1950, 'Fantasia', 978-0241968581, 'HarperCollins', 768, 'ingles');
+
+INSERT INTO Livros (titulo, autor, ano_publicacao, disponivel, categoria, isbn, editora, quantidade_paginas, idioma)
+VALUES ('Cem Anos de Solidão', 'Gabriel Garcia Marque', 1967, 'Ficçao', 978-0241968581, 'Penguin Books', 422, 'ingles')
+('Harry Potter e a Pedra Filosofal', 'J.K. Rowling', 1997, 'Fantasia', 978-0439708180, 'Bloomsbury', 309, 'ingles')
+('O Senhor dos Anéis: A Sociedade do Ane', 'J.R.R', 1954, 'Fantasia', 978-0618640157, 'HarperCollins', 423, 'ingles');
+
